89a705d5dcbbd5809485ddfd5c14a034a6e7e2b4
Emanuel Smith <emaasmithh@hotmail.com>
2024-05-29 21:59:34 -0300

    feat: CRUD de admin para Torneos

--- project/jugadores/admin.py
+++ project/jugadores/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
 from .models import Jugador
 
-admin.site.register(Jugador)
+
+
+class JugadorAdmin(admin.ModelAdmin):
+    list_display = (
+        "nombre",
+        "apellido",
+        "posicion",
+        "fecha_nacimiento",
+        "ranking",
+    )
+    list_display_links = ("nombre",)
+    list_filter = ("ranking",)
+
+admin.site.register(Jugador, JugadorAdmin)
