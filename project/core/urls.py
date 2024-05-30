d600885fa0b6a927951c9719c1e89bd0d5a58ad7
Emanuel Smith <emaasmithh@hotmail.com>
2024-05-29 21:45:40 -0300

    feat: Login , Register y Logout

--- project/core/urls.py
+++ project/core/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-
-from core.views import index
+from core.views import CustomLoginView, index, register
+from django.contrib.auth.views import LogoutView
 
 app_name = "core"
 
 urlpatterns = [
     path("", index, name="index"),
+    path("login/", CustomLoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(template_name="core/logout.html"), name="logout"),
+    path("register/", register, name="register"),
 ]
